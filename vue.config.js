0655d83f81ff6086221512290987bacefa40533b
Marjorie Waters <see_waters@comcast.net>
2024-05-05 18:41:40 -0400

    change title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
+
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'Weather Ahead!'; // Set your desired title here
+      return args;
+    });
+  }
+
 })
