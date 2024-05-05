f5a550adfb9283f443eaa96c7f031fd6fcfebf72
Marjorie Waters <see_waters@comcast.net>
2024-05-05 18:50:35 -0400

    update config file

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
-
-  chainWebpack: config => {
-    config.plugin('html').tap(args => {
-      args[0].title = 'Weather Ahead!'; // Set your desired title here
-      return args;
-    });
-  }
-
 })
